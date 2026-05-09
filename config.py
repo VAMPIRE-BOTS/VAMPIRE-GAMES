5e0e433d3523b6bcedad72b39814ede7f46ceca9
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-09 15:36:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,13 +14,13 @@ OWNER_USERNAME = getenv("OWNER_USERNAME", "yourusername")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 # SUPPORT INFO
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "yourgroup")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "yourchannel")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TOXIC_PARTY_OFFICIAL")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VAMPIREUPDATES")
 
 # MEDIA
 START_IMAGE = getenv(
     "START_IMAGE",
-    "https://i.imgur.com/yourimage.jpg"
+    "https://i.ibb.co/LdWsRnb8/x.jpg"
 )
 
 # LOGGER GROUP (NEW - for your logger system)
