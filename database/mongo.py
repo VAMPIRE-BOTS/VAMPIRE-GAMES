1f0ad25bd19109426b8f6962fa61a1d5cf817d3a
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-09 14:57:43 +0530

    Create mongo.py

---
+++ database/mongo.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI
+
+mongo = AsyncIOMotorClient(MONGO_DB_URI)
+
+db = mongo["ViralGamesBot"]
+
+usersdb = db["users"]
